10cc77bfab115a39a04243fc6c4aa5451cdcf56b
Alfred <alfredviktorov@gmail.com>
2022-09-16 21:11:39 +0300

    test

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-        <div>"Привет"</div>
+        <div>"Привет мир"</div>
       <App></App>
     </Provider>
   </React.StrictMode>
