67f2f5efe5a6af3a236a7001831aee1186a10ad9
Alfred <alfredviktorov@gmail.com>
2022-09-16 21:45:17 +0300

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import './index.css';
 
 const container = document.getElementById('root')!;
@@ -11,14 +7,7 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
         <div>"Привет мир"</div>
-      <App></App>
-    </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
